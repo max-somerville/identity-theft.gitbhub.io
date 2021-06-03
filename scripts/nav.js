999bbff948791ed9d629e84cf23f32f50407927c
max-somerville <84154962+max-somerville@users.noreply.github.com>
2021-06-03 16:12:28 +1200

    updates

---
+++ scripts/nav.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("header__nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("title").style.marginLeft = "150px";
+}
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("header__nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("title").style.marginLeft = "0";
+}
+
+
+
